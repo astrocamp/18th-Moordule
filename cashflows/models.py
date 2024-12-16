ae6a172a7e120517c3bff27c6e1b95774d004831
Joanna Lei <lovelilei21@gmail.com>
2024-12-16 17:14:52 +0800

    fix:default:models.DateTimeField(auto_now_add=True)

--- cashflows/models.py
+++ cashflows/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Payments(models.Model):
-    created_at = models.DateTimeField(),
+    created_at = models.DateTimeField(auto_now_add=True),
     amount = models.IntegerField(verbose_name="交易金額"),
     order_no = models.CharField(unique=True),
     # status = models.Choices(value=str),
 
 class Wallets(models.Model):
-    created_at = models.DateTimeField(),
+    created_at = models.DateTimeField(auto_now_add=True),
     balence = models.IntegerField(verbose_name="餘額"),
