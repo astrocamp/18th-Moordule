ae98a93d44f6c64d2df063c141e745e8e4d7bc43
YuHsinTengCindy <yuhsinteng@gmail.com>
2024-12-21 13:22:57 +0800

    fix: conflicts in setting.py

--- pydev/settings.py
+++ pydev/settings.py
@@ -143,7 +143,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
@@ -152,7 +151,6 @@ STATICFILES_DIRS = [
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
