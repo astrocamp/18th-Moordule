ca715d404619544763cbb995d8a57026ab4ee681
Tingan <syeak123@gmail.com>
2024-12-23 16:04:40 +0800

    fix:meetup_recommended_card

--- shared/templatetags/common_components.py
+++ shared/templatetags/common_components.py
@@ -11,3 +11,9 @@ def moordule_button(*, text, color):
 @register.inclusion_tag("shared/icon.html")
 def icon(*, name):
     return {"name": name}
+
+@register.inclusion_tag("shared/meetup_recommended_card.html")
+def meetup_recommended_card(meetup=None):
+    if meetup is None:
+        meetup = {}
+    return {"meetup": meetup}
