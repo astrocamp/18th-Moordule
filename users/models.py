d82b9d0b97e140c3e4c6738340ecc50c101ba3e8
richard <104408524+richart-coder@users.noreply.github.com>
2024-12-29 14:06:56 +0800

    feat: 註冊 CustomUser 於後台介面

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 # Create your models here.
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 # Create your models here.
@@ -17,36 +17,53 @@ class Hobby(models.Model):
         return self.name
 
 
+class CustomUserManager(UserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        # 因為 AbstractUser 需要 username，所以我們用 email 當作 username
+        return super().create_user(
+            username=email,  # username 欄位還是需要值
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+
 class CustomUser(AbstractUser):
-    
+
     GENDER_CHOICES = [
-        ('male', 'Male'),  # ('存儲值', '顯示值')
-        ('female', 'Female'),
-        ('other', 'Other'),
+        ("male", "Male"),  # ('存儲值', '顯示值')
+        ("female", "Female"),
+        ("other", "Other"),
     ]
 
     HOBB_CHOICES = [
-        ('eating', '吃飯'),
-        ('drinking', '喝酒'),
-        ('sleeping', '唱歌'),
-        ('sports', '運動'),
-        ('movies', '電影'),
-        ('discussion', '討論'),
+        ("eating", "吃飯"),
+        ("drinking", "喝酒"),
+        ("sleeping", "唱歌"),
+        ("sports", "運動"),
+        ("movies", "電影"),
+        ("discussion", "討論"),
     ]
     username = models.CharField(max_length=150, blank=True, null=True)
     email = models.EmailField(unique=True)
     avatar_url = models.CharField(max_length=255, blank=True, null=True)
-    birth_date= models.DateField(blank=True, null=True)
-    gender= models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, blank=True, null=True
+    )
     live_in = models.CharField(max_length=255, blank=True, null=True)
-    
-    hobby = models.CharField(max_length=255, choices=HOBB_CHOICES, blank=True, null=True)
+
+    hobby = models.CharField(
+        max_length=255, choices=HOBB_CHOICES, blank=True, null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     google_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-
+    objects = CustomUserManager()
 
     class Meta:
         db_table = "users"
