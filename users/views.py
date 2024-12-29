4b666ffa04d7541320be77ffac8d2a9bb5806311
richard <104408524+richart-coder@users.noreply.github.com>
2024-12-29 22:11:29 +0800

    feat(user): 實作修改密碼功能

--- users/views.py
+++ users/views.py
@@ -1,16 +1,47 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.cache import never_cache
 from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
 
 from users.models import CustomUser
 
-from .forms import UserRegistrationForm, CustomUserChangeForm
+from .forms import CustomUserChangeForm, UserRegistrationForm
 
 # Create your views here.
+
+
+@login_required
+def member_view(request: HttpRequest):
+    return render(request, "users/account.html")
+
+
+@login_required
+def password_change_view(request):
+    if request.method == "GET":
+        return render(request, "users/components/password_change_form.html")
+    else:
+        user = request.user
+        old_password = request.POST.get("old_password")
+        new_password = request.POST.get("new_password1")
+        confirm_password = request.POST.get("new_password2")
+
+        if not user.check_password(old_password):
+            return HttpResponse(
+                '<p class="text-red-500 text-sm mt-1">舊密碼輸入有誤</p>'
+            )
+
+        if new_password != confirm_password:
+            return HttpResponse('<p class="text-red-500 text-sm mt-1">密碼不一致</p>')
+
+        user.set_password(new_password)
+        user.save()
+
+        update_session_auth_hash(request, user)
+        account_url = reverse("users:account")
+        return HttpResponse("", headers={"HX-Redirect": account_url})
 
 
 def signup_view(request: HttpRequest):
@@ -137,10 +168,12 @@ def login_view(request: HttpRequest):
 def clear_errors(request):
     return HttpResponse("")
 
+
 @login_required
 def account_view(request: HttpRequest):
     user = CustomUser.objects.get(pk=request.user.pk)
-    return render(request, "users/member.html", {"user": user})    
+    return render(request, "users/member.html", {"user": user})
+
 
 @login_required
 def edit_view(request: HttpRequest):
@@ -152,12 +185,9 @@ def edit_view(request: HttpRequest):
         form = CustomUserChangeForm(request.POST, instance=user)
         if form.is_valid():
             # 如果表單數據有效，保存用戶資料
-            form.save()  
+            form.save()
     else:
         # 如果是 GET 請求，顯示當前用戶數據
         form = CustomUserChangeForm(instance=user)
 
     return render(request, "users/components/user_form.html", {"form": form})
-
-    
-
