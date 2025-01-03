3ebb16541b3187ec9162517e59f5931ff8f94dc5
Tingan <syeak123@gmail.com>
2025-01-03 15:31:29 +0800

    feat:add aws s3

--- moordule/settings.py
+++ moordule/settings.py
@@ -169,3 +169,23 @@ LINE_REQUEST_URL = os.getenv("LINE_REQUEST_URL")
 LINE_SANDBOX_URL = os.getenv("LINE_SANDBOX_URL")
 
 GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = os.getenv("MEDIA_URL") 
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
+            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+            "bucket_name": os.getenv("AWS_STORAGE_BUCKET_NAME"),
+            "region_name": os.getenv("AWS_S3_REGION_NAME"),
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+AWS_S3_FILE_OVERWRITE = False 
+AWS_DEFAULT_ACL = None
