05c74e531386a83600fff6790104066b33129bed
JoannaLei21 <lovelilei21@gmail.com>
2025-01-04 20:12:38 +0800

    fix:remove doubled code from main.js

--- src/script/main.js
+++ src/script/main.js
@@ -7,23 +7,3 @@ import initMap from './map';
 initMap(); 
 
 Alpine.start();
-
-window.initMap = function() {
-    const address = document.getElementById('address').value;
-    const geocoder = new google.maps.Geocoder();
-    
-    geocoder.geocode({ 'address': address }, function(results, status) {
-        if (status === 'OK') {
-            const map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 15,
-                center: results[0].geometry.location
-            });
-            new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map
-            });
-        } else {
-            console.error('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-};
