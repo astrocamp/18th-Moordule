6e55e6bead6d09bdc32359a3d9bb3ae295e52ef2
YuHsinTengCindy <yuhsinteng@gmail.com>
2025-01-07 14:26:48 +0800

    fix:delete redundant verification

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 from django.shortcuts import render
 from django.utils import timezone
-from activities.views import activities
 from activities.models import Activity
 
 # Create your views here.
@@ -18,12 +17,5 @@ def index_view(request):
     context = {
         "upcoming_activities": upcoming_activities,
     }
-
-    activities_context = activities(request)
-
-    if isinstance(activities_context, dict):
-        context.update(activities_context)
-    else:
-        print(f"activities(request) 返回的結果不是字典，而是：{activities_context}")
 
     return render(request, "pages/index.html", context)
