f80c894f9fe653586ad1fc8e7612d0ceff93cc34
Tingan <syeak123@gmail.com>
2025-01-08 20:50:56 +0800

    fix: googlemap

--- src/script/main.js
+++ src/script/main.js
@@ -2,6 +2,4 @@ import Alpine from "alpinejs";
 import htmx from "htmx.org";
 import zxcvbn from "zxcvbn";
 window.zxcvbn = zxcvbn;
-import initMap from "./map";
-window.initMap = initMap;
 Alpine.start();
