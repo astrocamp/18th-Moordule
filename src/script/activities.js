28bd7f3f4cb4ba34ccf4338f83b39aa6b30d09c3
Tingan <syeak123@gmail.com>
2025-01-14 10:06:48 +0800

    fix:activites preview

--- src/script/activities.js
+++ src/script/activities.js
@@ -20,3 +20,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
     });
+    document.getElementById('photo').addEventListener('change', function(event) {
+        const file = event.target.files[0]; 
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const preview = document.getElementById('preview');
+                preview.src = e.target.result;
+                preview.style.display = 'block'; 
+            };
+            reader.readAsDataURL(file); 
+        }
+    });
