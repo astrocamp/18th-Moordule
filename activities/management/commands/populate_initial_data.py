6da8aad3dc3d81b63b22f38cb44caeddb2945035
Emma-EC <chensyec@gmail.com>
2025-01-14 11:02:20 +0800

    fix: remove initial data and migrations

--- activities/management/commands/populate_initial_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from activities.models import MyModel
-
-class Command(BaseCommand):
-    help = 'Populate initial data'
-
-    def handle(self, *args, **kwargs):
-        MyModel.objects.create(name='Example', value=123)
-        self.stdout.write(self.style.SUCCESS('Successfully populated initial data'))
