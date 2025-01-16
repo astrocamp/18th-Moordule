72c8f54e25e632619b64f8b52d63388fc2c8df39
JoannaLei21 <lovelilei21@gmail.com>
2025-01-16 21:01:58 +0800

    feat:3 plans checkout

--- cashflows/urls.py
+++ cashflows/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = "cashflows"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("request/", views.request_payment, name="request_payment"),
+    path("request_b/", views.request_payment_b, name="request_payment_b"),
+    path("request_a/", views.request_payment_a, name="request_payment_a"),
+    path("request_c/", views.request_payment_c, name="request_payment_c"),
+    path("payment/confirm_a", views.confirm_a, name="confirm_a"),
     path("payment/confirm_b", views.confirm_b, name="confirm_b"),
+    path("payment/confirm_c", views.confirm_c, name="confirm_c"),
 ]
--- cashflows/views.py
+++ cashflows/views.py
@@ -47,7 +47,104 @@ def create_headers(body, uri):
 
 # line pay API body
 @login_required
-def request_payment(request):
+def request_payment_a(request):
+    if request.method == "POST":
+        # 設定訂單與付款資訊
+        order_id = f"order_{str(uuid.uuid4())}"
+        package_id = f"package_{str(uuid.uuid4())}"
+        payload = {
+            "amount": 199,
+            "currency": "TWD",
+            "orderId": order_id,
+            "packages": [
+                {
+                    "id": package_id,
+                    "amount": 199,
+                    "products": [
+                        {
+                            "id": "1",
+                            "name": "方案A 想試揪",
+                            "quantity": 1,
+                            "price": 199,
+                        }
+                    ],
+                }
+            ],
+            "redirectUrls": {
+                "confirmUrl": f"https://{settings.HOSTNAME}/cashflows/payment/confirm_a",
+                "cancelUrl": f"https://{settings.HOSTNAME}/payment/cancel",
+            },
+        }
+
+        # 發送 API 請求
+        uri = settings.LINE_REQUEST_URL
+        headers = create_headers(payload, uri)
+        url = f"{settings.LINE_SANDBOX_URL}{uri}"
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+        # 根據狀態，處理回應
+        if response.status_code == 200:
+            data = response.json()
+            if data["returnCode"] == "0000":
+                # 儲存付款資訊至資料庫，並關聯到當前用戶
+                Payment.objects.create(
+                    user=request.user,  # 將當前用戶儲存到 Payment 中
+                    order_id=order_id,
+                    transaction_id=data["info"]["transactionId"],  # 假設這裡有交易ID
+                    amount=199,
+                    currency="TWD",
+                    status="待確認"  # 初始狀態可以設為待確認
+                )
+                return redirect(data["info"]["paymentUrl"]["web"])
+            else:
+                return render(
+                    request, "payment/error.html", {"message": data["returnMessage"]}
+                )
+        else:
+            return render(
+                request,
+                "payment/error.html",
+                {"message": f"Error: {response.status_code}"},
+            )
+
+    return render(request, "payment/checkout_a.html")
+
+def confirm_a(request):
+
+    try:
+        transaction_id = request.GET.get("transactionId")  
+        
+        if not transaction_id:
+            return render(request, "payment/error.html", {"message": "Missing required parameters."})
+
+        # 根據 transaction_id 查找對應的支付記錄
+        payment = Payment.objects.filter(transaction_id=transaction_id).first()
+        if not payment:
+            return render(request, "payment/error.html", {"message": "Payment record not found."})
+
+        
+        payment.status = "已付款"
+        payment.save()  
+
+        balance = Wallet.objects.filter(user=payment.user).first()
+        bouns_a = 50
+        if balance:
+            balance.balence += payment.amount + bouns_a
+            balance.save()  
+        else:
+            Wallet.objects.create(user=payment.user, balence=payment.amount)  # 新增餘額
+
+        user = payment.user
+        user.membership_level = "Silver"
+        user.save()  
+        
+        return render(request, "payment/confirm_a.html", {"data": payment})
+
+    except Exception as e:
+        return render(request, "payment/error.html", {"message": f"Exception: {str(e)}"})
+
+@login_required
+def request_payment_b(request):
     if request.method == "POST":
         # 設定訂單與付款資訊
         order_id = f"order_{str(uuid.uuid4())}"
@@ -143,5 +240,103 @@ def confirm_b(request):
 
     except Exception as e:
         return render(request, "payment/error.html", {"message": f"Exception: {str(e)}"})
-
       
+
+    #   
+
+@login_required
+def request_payment_c(request):
+    if request.method == "POST":
+        # 設定訂單與付款資訊
+        order_id = f"order_{str(uuid.uuid4())}"
+        package_id = f"package_{str(uuid.uuid4())}"
+        payload = {
+            "amount": 1399,
+            "currency": "TWD",
+            "orderId": order_id,
+            "packages": [
+                {
+                    "id": package_id,
+                    "amount": 1399,
+                    "products": [
+                        {
+                            "id": "1",
+                            "name": "方案A 想試揪",
+                            "quantity": 1,
+                            "price": 1399,
+                        }
+                    ],
+                }
+            ],
+            "redirectUrls": {
+                "confirmUrl": f"https://{settings.HOSTNAME}/cashflows/payment/confirm_c",
+                "cancelUrl": f"https://{settings.HOSTNAME}/payment/cancel",
+            },
+        }
+
+        # 發送 API 請求
+        uri = settings.LINE_REQUEST_URL
+        headers = create_headers(payload, uri)
+        url = f"{settings.LINE_SANDBOX_URL}{uri}"
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+        # 根據狀態，處理回應
+        if response.status_code == 200:
+            data = response.json()
+            if data["returnCode"] == "0000":
+                # 儲存付款資訊至資料庫，並關聯到當前用戶
+                Payment.objects.create(
+                    user=request.user,  # 將當前用戶儲存到 Payment 中
+                    order_id=order_id,
+                    transaction_id=data["info"]["transactionId"],  # 假設這裡有交易ID
+                    amount=1399,
+                    currency="TWD",
+                    status="待確認"  # 初始狀態可以設為待確認
+                )
+                return redirect(data["info"]["paymentUrl"]["web"])
+            else:
+                return render(
+                    request, "payment/error.html", {"message": data["returnMessage"]}
+                )
+        else:
+            return render(
+                request,
+                "payment/error.html",
+                {"message": f"Error: {response.status_code}"},
+            )
+
+    return render(request, "payment/checkout_c.html")
+
+def confirm_c(request):
+
+    try:
+        transaction_id = request.GET.get("transactionId")  
+        
+        if not transaction_id:
+            return render(request, "payment/error.html", {"message": "Missing required parameters."})
+
+        # 根據 transaction_id 查找對應的支付記錄
+        payment = Payment.objects.filter(transaction_id=transaction_id).first()
+        if not payment:
+            return render(request, "payment/error.html", {"message": "Payment record not found."})
+
+        
+        payment.status = "已付款"
+        payment.save()  
+
+        balance = Wallet.objects.filter(user=payment.user).first()
+        bouns_a = 200
+        if balance:
+            balance.balence += payment.amount + bouns_a
+            balance.save()  
+        else:
+            Wallet.objects.create(user=payment.user, balence=payment.amount)  # 新增餘額
+
+        user = payment.user
+        user.membership_level = "Platinum"
+        user.save()  
+        
+        return render(request, "payment/confirm_c.html", {"data": payment})
+
+    except Exception as e:
+        return render(request, "payment/error.html", {"message": f"Exception: {str(e)}"})
