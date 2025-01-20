cc7a2dc12dd3be7490ddd922b39760a6b8470a37
Tingan <syeak123@gmail.com>
2025-01-20 18:02:41 +0800

    fix:delete annotation

--- cashflows/views.py
+++ cashflows/views.py
@@ -21,7 +21,7 @@ def index(request):
 def create_headers(body, uri):
     channel_id = settings.LINE_CHANNEL_ID
     secret_key = settings.LINE_CHANNEL_SECRET_KEY
-    nonce = str(uuid.uuid4())  # TODO:確認使用nonc是用uuid?
+    nonce = str(uuid.uuid4())
     # header 轉換成json格式
     body_to_json = json.dumps(body)
 
